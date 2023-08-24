dddac533d9ddaaac949f9b72e59a5a9e62633877
pinke <pinke.yu7@gmail.com>
2023-08-24 17:41:43 +0800

    Feature: add example code

---
+++ example/Program.cs
@@ -0,0 +1,22 @@
+﻿using YatingTtsClient;
+using YatingTtsClient.Constants;
+
+string ttsApiUrl = "TTS_ENDPOINT";
+string ttsApiKey = "PUT_YOUR_API_KEY_HERE";
+
+string inputText = "歡迎使用雅婷文字轉語音。";
+string inputType = InputType.Text;
+string voiceModel = VoiceModel.Female2;
+string audioEncoding = AudioEncoding.Linear16;
+string audioSampleRate = AudioSampleRate.SR16k;
+string fileName = "example";
+
+try
+{
+    TtsClient ttsClient = new TtsClient(ttsApiUrl, ttsApiKey);
+    ttsClient.Synthesize(inputText, inputType, voiceModel, audioEncoding, audioSampleRate, fileName);
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
